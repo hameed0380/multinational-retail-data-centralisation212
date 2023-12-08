722670dfe9834a3b51032001503a5696c46630d9
Hameed Roleola <hameed0380@gmail.com>
2023-12-08 00:43:02 +0000

    added engine init and method to retrieve list of table names

--- database_utils.py
+++ database_utils.py
@@ -1,2 +1,42 @@
+import yaml
+from sqlalchemy import create_engine, inspect
+import pandas as pd
+
 class DatabaseConnector:
-    pass
+    
+    def __init__(self, credential) -> None:
+        self.loaded_creds = self.read_db_creds(credential)
+        self.engine = self.init_db_engine()
+
+
+    def read_db_creds(self,credential):
+
+        with open(credential, 'r') as file:
+            credential = yaml.safe_load(file)
+        #print(credential)
+        return credential
+    
+    def init_db_engine(self):
+        creds = self.loaded_creds
+
+        DATABASE_TYPE = 'postgresql'
+        HOST = creds["RDS_HOST"]
+        PASSWORD = creds["RDS_PASSWORD"]
+        USER = creds["RDS_USER"]
+        DATABASE = creds["RDS_DATABASE"]
+        PORT = 5432
+
+        engine = create_engine(f"{DATABASE_TYPE}://{USER}:{PASSWORD}@{HOST}:{PORT}/{DATABASE}")
+        engine.execution_options(isolation_level='AUTOCOMMIT').connect()
+        return engine
+    
+    def list_db_tables(self):
+        inspector = inspect(self.engine)
+        result = inspector.get_table_names()
+        print(result)
+        return result
+    
+if __name__ == '__main__':
+    RDS_CONNECTOR = DatabaseConnector('db_creds.yaml')
+    RDS_CONNECTOR.init_db_engine()
+    RDS_CONNECTOR.list_db_tables()
