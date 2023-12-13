972aec4f5942a2e225dc553d126b1ff6e969c3e6
Hameed Roleola <hameed0380@gmail.com>
2023-12-13 01:42:48 +0000

    added a method to get pages from a pdf using tabula

--- data_extraction.py
+++ data_extraction.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from database_utils import DatabaseConnector
+import tabula
 
 
 class DataExtractor:
@@ -19,6 +20,15 @@ class DataExtractor:
         df = pd.DataFrame(data)
         return df
     
+    def retrieve_pdf_data(self, link):
+        '''
+        Extract all pages from the pdf document
+        '''
+        dfs = tabula.read_pdf(link, pages='all')
+        df = pd.concat(dfs)
+        return df
+    
+
 
 RDS_CONNECTOR = DatabaseConnector()
 RDS_CONNECTOR.init_db_engine()
@@ -29,3 +39,5 @@ data = RDS_CONNECTOR.init_db_engine()
 lamb = DataExtractor()
 # ['legacy_store_details', 'legacy_users', 'orders_table']
 print(lamb.read_rds_table('legacy_users'))
+k = lamb.retrieve_pdf_data('https://data-handling-public.s3.eu-west-1.amazonaws.com/card_details.pdf')
+print(k)
