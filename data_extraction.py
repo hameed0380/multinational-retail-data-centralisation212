7d03fe595d83e7f971636cc3ebc0a1e336f5c525
Hameed Roleola <hameed0380@gmail.com>
2023-12-19 23:40:50 +0000

    added extraction method to retrieve from s3 bucket

--- data_extraction.py
+++ data_extraction.py
@@ -1,8 +1,10 @@
 import pandas as pd
-from database_utils import DatabaseConnector
 import tabula
 import boto3
 import requests
+import io
+import re
+from database_utils import DatabaseConnector
 
 
 class DataExtractor:
@@ -33,8 +35,8 @@ class DataExtractor:
     
     def list_number_of_stores(self, endpoint, headers):
         '''
-        The function `retrieve_stores_data` retrieves data from a specified URL endpoint for a given number 
-        of stores and returns a concatenated dataframe of all the retrieved data.
+        Function makes a GET request to a specified URL endpoint with headers,
+        retrieves the response data as JSON, and returns the number of stores from the response data
 
         Args:
             endpoint: URL endpoint where the store data is retrieved from
@@ -78,9 +80,83 @@ class DataExtractor:
         full_df = pd.concat(combined_df, ignore_index=True)
 
         return full_df
+    
+    def get_file_extension(self, s3_address):
+        '''
+        Retrieve anything trailling . in the url to retrieve the files extension rather then hard coding
+        Args:
+            s3 address: 
+        '''
+        delimiter = "."
+        parts = s3_address.split(delimiter)
+        extension_retrieved = parts[-1]
+        return extension_retrieved
+    
+    def extract_url_without(self, s3_address):
+        # regex pattern to match everything before the second forward slash
+        pattern = re.compile(r'^(?:[^/]+://)?([^/]+)/(.*)$')
 
-    def extract_from_s3(self):
-        pass
+        # Match pattern of regex
+        match_pattern = pattern.match(s3_address)
+        bucket = match_pattern.group(1)
+        key = match_pattern.group(2)
+        print(bucket, key)
+        return bucket, key
+
+    
+    def extract_from_s3(self, s3_address):
+        print(f"Downloading file from {s3_address}...")
+        # Initialize the S3 client
+        s3 = boto3.client('s3')
+
+        file_extension = self.get_file_extension(s3_address)
+        # Splitting the S3 address to get the bucket and key
+
+        #s3_address 
+        bucket, key = self.extract_url_without(s3_address=s3_address)
+        print(bucket, key)
+        #bucket, key = s3_address.split('/', 1)
+
+        if file_extension == 'csv':
+        # # Download the file from S3
+        #     with open('products.csv', 'wb') as file:
+        #         s3.download_fileobj(bucket, key, file)
+
+        #     print("File has sucessfully downloaded.")
+        #     print("Reading the CSV file into DataFrame...")
+        #     # Read the CSV file into a DataFrame
+        #     df = pd.read_csv('products.csv')
+
+            response = s3.get_object(Bucket=bucket, Key=key)
+            content = response['Body'].read()
+    
+            # Create a Pandas DataFrame from the CSV data
+            df = pd.read_csv(io.BytesIO(content))
+            return df
+        
+
+        elif file_extension == 'json':
+            # Download the file from S3
+            # with open('products.csv', 'wb') as file:
+            #     s3.download_fileobj(bucket, key, file)
+
+            # print("File has sucessfully downloaded.")
+            # print("Reading the CSV file into DataFrame...")
+            # # Read the CSV file into a DataFrame
+            # df = pd.read_csv('products.csv')
+
+            response = s3.get_object(Bucket=bucket, Key=key)
+            content = response['Body'].read()
+            
+            df = pd.read_json(io.BytesIO(content))
+            return df
+        else:
+            print('Print error unsupported format')
+
+        print("DataFrame created ")
+
+
+
     
 
 if __name__ == '__main__':
