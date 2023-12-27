042bf2d2bf58bb20ab6c97f92f71217025ed7d5c
Hameed Roleola <hameed0380@gmail.com>
2023-12-27 16:02:44 +0000

    refactored databa

--- database_utils.py
+++ database_utils.py
@@ -9,7 +9,8 @@ class DatabaseConnector:
 
 
     def read_db_creds(self):
-        '''Load credentials file to access the database
+        '''
+        Load credentials file to access the database
         
         Args:
             credential: path of the credentials file to connect to the database
@@ -23,20 +24,36 @@ class DatabaseConnector:
             return credential
     
     def init_db_engine(self):
-        '''initialises a SQLAlchemy engine from the credentials provided from class'''
+        '''
+        Initialises a SQLAlchemy engine from the credentials provided from class
+        
+        return:
+            engine: the instance of the create_engine class
+        '''
         engine = create_engine(f"postgresql://{self.read_db_creds()['RDS_USER']}:{self.read_db_creds()['RDS_PASSWORD']}@{self.read_db_creds()['RDS_HOST']}:{self.read_db_creds()['RDS_PORT']}/{self.read_db_creds()['RDS_DATABASE']}")        
         engine.execution_options(isolation_level='AUTOCOMMIT').connect()
         return engine
     
     def list_db_tables(self):
-        '''Retrieves the names of the tables'''
+        '''
+        Retrieves the names of the tables
+
+        return:
+            retrieved_names: retrieved name of the tables from db
+        '''
         inspector = inspect(self.engine)
         retrieved_names = inspector.get_table_names()
         print(retrieved_names)
         return retrieved_names
     
     def upload_to_db(self, df, tablename):
-        '''Creates and uploads table to database'''
+        '''
+        Creates and uploads table to database
+        
+        args:
+            df: the dataframe to be uploaded to the db
+            tablename: establish name of table
+        '''
         engine = create_engine(f"postgresql://{self.read_db_creds()['PS_USER']}:{self.read_db_creds()['PS_PASSWORD']}@{self.read_db_creds()['PS_HOST']}:{self.read_db_creds()['PS_PORT']}/{self.read_db_creds()['PS_DATABASE']}")        
         df.to_sql(tablename, engine, if_exists='replace', index=False)
         print("UPLOAD SUCCESSFUL")
